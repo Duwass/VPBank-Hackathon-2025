e459638ab4d5623d96972005c13761b787075e96
brooklyng3 <brooklyng3.uni@gmail.com>
2025-07-16 04:49:30 +0700

    Update script.ps1: Add windows server compartability and report generation

--- script.ps1
+++ script.ps1
@@ -4,12 +4,20 @@ Import-Module -Name kbupdate -Force
 Import-Module -Name MsrcSecurityUpdates -Force
 
 $osver = Get-ComputerInfo | Select-Object OSName
-$osver = $osver -replace '\D+'
-$osyear = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
-$fullos = "Windows " + $osver + " Version " + $osyear + " for x64-based Systems"
+if($osver -match "server"){
+    $fullos = $matches[0]
+}
+else{
+    $osver = $osver -replace '\D+'
+    $osyear = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
+    $fullos = "Windows " + $osver + " Version " + $osyear + " for x64-based Systems"
+}
+
 $date = Get-Date -UFormat "%Y-%b"
 
-$cvrfDoc = Get-MsrcCvrfDocument -ID $date
+mkdir c:\temp\reports
+$cvrfDoc = Get-MsrcCvrfDocument -ID $date -Verbose
+$cvrfDoc | Get-MsrcSecurityBulletinHtml -Verbose | Out-File c:\temp\reports\"MSRC$($date)SecurityUpdates.html"
 $productID = $cvrfDoc.ProductTree.FullProductName | Where-Object { $_.Value -eq $fullos } | Select-Object -ExpandProperty ProductID
 
 $max = ($cvrfDoc.Vulnerability.Remediations | Where ProductID -EQ $productID | where FixedBuild -Match '\w+' |Measure-Object -Property Supercedence -Maximum).Maximum
@@ -20,8 +28,10 @@ $url = $obj.URL
 $uri = [System.Uri]$url
 
 $kbver = [System.Web.HttpUtility]::ParseQueryString($uri.Query).Get("q")
-mkdir C:\tmp\logs
+mkdir C:\temp\logs
 
 Install-KbUpdate -HotfixId $kbver -ComputerName $env:computername | Out-File C:\tmp\logs\"log_update_$kbver.txt"
 
 Get-KbNeededUpdate -ComputerName $env:computername | Install-KbUpdate |Out-File C:\tmp\logs\"log_update_needed.txt"
+
+Restart-Computer
