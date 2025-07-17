a43062be3ab9dd4104106076a63bb6bacd966922
brooklyng3 <brooklyng3.uni@gmail.com>
2025-07-17 20:09:41 +0700

    add update log

--- script.ps1
+++ script.ps1
@@ -14,7 +14,7 @@ if($osver -match "server"){
 
 $date = Get-Date -UFormat "%Y-%b"
 
-Remove-Item -LiteralPath "C:\temp" -Force -Recurse
+Remove-Item -LiteralPath "C:\temp" -Force -Recurse -ErrorAction SilentlyContinue
 mkdir c:\temp\reports
 $cvrfDoc = Get-MsrcCvrfDocument -ID $date -Verbose
 $cvrfDoc | Get-MsrcSecurityBulletinHtml -Verbose | Out-File c:\temp\reports\"MSRC$($date)SecurityUpdates.html"
@@ -33,5 +33,7 @@ mkdir C:\temp\logs
 Install-KbUpdate -HotfixId $kbver -ComputerName $env:computername | Out-File C:\tmp\logs\"log_update_$kbver.txt"
 
 Get-KbNeededUpdate -ComputerName $env:computername | Install-KbUpdate |Out-File C:\tmp\logs\"log_update_needed.txt"
+
+Get-WindowsUpdateLog -LogPath C:\temp\logs\WindowsUpdate.log
 
 Restart-Computer
